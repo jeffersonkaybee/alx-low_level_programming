406d0a14b3273d62133d4108e60204059205c8a0
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-09 16:47:34 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+
+/**
+ * * main - Entry point
+ * *
+ * * Author: Sammykingx
+ * *
+ * * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	char string[70] = "\"Programming is like building a multilingual puzzle\"";
+
+		puts(string):
+
+			/* Tried printing it directly */
+
+			puts("\"Programming is like building a multilingual puzzle\"");
+		return (0);
+}
