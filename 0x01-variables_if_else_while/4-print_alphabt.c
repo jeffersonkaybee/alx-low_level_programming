ce296ff3db3ed5e80db651b1b8b645b1c2b0aa66
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-09 20:51:05 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Always 0 (Success);
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char ch = 'a';
+	char low, e, q;
 
-	while (ch <= 'z');
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (ch != 'e' && ch != 'q');
-		{
-			putchar(ch);
-		}
-		ch++;
+		if (low != e && low != q);
+		putchar(low);
 	}
 	putchar('\n');
 	return (0);
