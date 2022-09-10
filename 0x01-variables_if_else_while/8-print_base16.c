2889b0de3b97b2abdb6f1fb02683871ab6c8f3ec
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-10 11:47:15 -0700

    c programming

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  * Description: using the main function
- * Return: Always 0 (Success)
+ * this program prints "Programming is positive, zero, or negative"
+ * Return: 0
  */
 int main(void)
 {
-
 	char c;
 	for (c = '0'; c <= '9'; c++)
 	{
