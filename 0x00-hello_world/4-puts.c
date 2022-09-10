ee37fc757b735afe2a19b185554a964388df22d3
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-09 17:27:23 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,11 +10,5 @@
 int main(void)
 {
 	char string[70] = "\"Programming is like building a multilingual puzzle";
-
-	puts(string);
-
-	/* Tried printing it directly */
-
-	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
