814fd9192e880df78b5050c870c9826f0be58d2a
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-10 12:17:21 -0700

    c programming

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
  * main - Entry point
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative"
  * Return: Always 0 (Success);
  */
 
 int main(void);
 {
-	char low;
-	for (low = 'z'; low >= 'a'; low--);
-	putchar(low);
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--);
+	{
+		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
