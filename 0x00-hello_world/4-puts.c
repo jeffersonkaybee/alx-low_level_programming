75b6ebb718d0f67c88f0092e6c03160f141b1fbe
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-09 17:01:52 -0700

    up to date c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- * * main - Entry point
- * *
- * * Author: Sammykingx
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - Entry point
+ *
+ * Author: Sammykingx
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char string[70] = "\"Programming is like building a multilingual puzzle\"";
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %1u byte(s)\n", sizeof(char));
