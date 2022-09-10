621bb6af4478231d96802fa4413e3b20878ef0ed
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-09 20:11:05 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * Author: JeffersonKaybee
  * Date: 09-10-2022
- * Details: c program to print +ve, zero and -ve results of a random number
+ * Details: c program to print (positive, zero and negative results of a random number);
  * Return: 0 (success)
  */
 int main(void)
@@ -18,16 +18,16 @@ int main(void)
 	 * your code goes there
 	 */ 
 	if (n < 0)
-		{	
-			printf("%d is negative\n", n);
-		}
-		else if (n > 0)
-		{
-			printf("%d is positive\n", n);
-		}
-		else
-		{
-			printf("%d is zero\n", n);
-		}
+	{	
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
