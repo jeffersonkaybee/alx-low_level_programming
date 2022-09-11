cc21365ac3f7964350d99b8078234514010a951b
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-11 13:18:33 -0700

    c programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - program compilation begins from main
- * Description: Program to print all possible combinations of numbers with no two numbers same in pairing
  * author: jeffersonkaybee
  * Return: Always 0 (Success);
  */
@@ -9,11 +8,11 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 48; a <=57; a++)
+	for (a = 48; a <= 57; a++)
 	{
 		for (b = 49; b <= 57; b++)
 		{
-			if ( a != b && b > a)
+			if (a != b && b > a)
 			{
 				putchar(a);
 				putchar(b);
