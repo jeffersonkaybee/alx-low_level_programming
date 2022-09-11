9d6d216e033551cac1660bddd39b12d4a2c10c79
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-11 13:14:34 -0700

    c programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 48; a <=57; a++);
+	for (a = 48; a <=57; a++)
 	{
-		for (b = 49; b <= 57; b++);
+		for (b = 49; b <= 57; b++)
 		{
-			if ( a != b && b > a);
+			if ( a != b && b > a)
 			{
-				putchar(a );
-				putchar(b );
+				putchar(a);
+				putchar(b);
 				putchar(',');
 			}
 		}
