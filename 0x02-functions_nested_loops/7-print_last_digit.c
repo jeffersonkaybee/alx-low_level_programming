4dc34cfe52e30ab79195271fbf0bf153d989454f
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-13 19:51:55 -0700

    programming in c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,17 @@
  * author: jeffersonkaybee
  * Return: returns the last digit to calling function
  */
-int print_last_digit(int a)
+int print_last_digit(int n)
 {
-	if (a >= 0 && a <= 9)
-		return (a);
+	int a;
 
-	else
-	{
-		a = _abs(a % 10);
-		return (a);
-	}
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+	return(a);
 }
