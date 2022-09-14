d30fe2c39c2a4fde4856472d5e3e57b6c74c36d3
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-14 09:37:23 -0700

    c codes

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * * main - causes an infinite loop
  * * Return: 0
  * */
-
 int main(void)
 {
 	int i;
@@ -19,5 +17,6 @@ int main(void)
 		/*}*/
 
 		printf("Infinite loop avoided! \\o/\n");
+
 		return (0);
 }
