1bdcfd387a7a8f89f9bd358f20ae6c990f4e5299
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-13 18:00:17 -0700

    programming in c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,6 +12,11 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
 		_putchar('0');
 	}
 	else
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,7 +9,6 @@
 int _abs(int n)
 {
 	if (n > 0)
-		return (n);
 	else
 	{
 		n *= -1;
