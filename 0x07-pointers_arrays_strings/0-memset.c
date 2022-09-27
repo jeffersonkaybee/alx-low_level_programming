6dd764edbf07762828ca74db745ae4d40113b19a
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-26 17:36:00 -0700

    c programming

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,6 @@
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
- *
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,18 @@
  */
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
+	int a;
+	
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
 		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
+			return (s - 1);
 		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
 }
