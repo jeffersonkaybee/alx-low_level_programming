64fd5a72f31632f9fca01148eb6199625325c714
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-30 16:55:46 -0700

    c codes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,41 +8,41 @@
  */
 int _atoi(char *s)
 {
-  int i, d, n, len, f, digit;
+	int i, d, n, len, f, digit;
 
-  i = 0;
-  d = 0;
-  n = 0;
-  len = 0;
-  f = 0;
-  digit = 0;
+  	i = 0;
+  	d = 0;
+  	n = 0;
+  	len = 0;
+  	f = 0;
+  	digit = 0;
 
-  while (s[len] != '\0')
-    len++;
+  	while (s[len] != '\0')
+		len++;
 
-  while (i < len && f == 0)
-    {
-      if (s[i] == '-')
-	++d;
+  	while (i < len && f == 0)
+    	{
+      		if (s[i] == '-')
+			++d;
 
-      if (s[i] >= '0' && s[i] <= '9')
-	{
-	  digit = s[i] - '0';
-	  if (d % 2)
-	    digit = -digit;
-	  n = n * 10 + digit;
-	  f = 1;
-	  if (s[i + 1] < '0' || s[i + 1] > '9')
-	    break;
-	  f = 0;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-      i++;
-    }
 
-  if (f == 0)
-    return (0);
+	if (f == 0)
+		return (0);
 
-  return (n);
+	return (n);
 }
 
 /**
@@ -53,31 +53,31 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-  int sum, num, i, j, k;
+	int sum, num, i, j, k;
 
-  sum = 0;
+	sum = 0;
 
-  for (i = 1; i < argc; i++)
-    {
-      for (j = 0; argv[i][j] != '\0'; j++)
+	for (i = 1; i < argc; i++)
 	{
-	  if (argv[i][j] > '9' || argv[i][j] < '0')
-	    {
-	      puts("Error");
-	      return (1);
-	    }
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
 	}
-    }
 
-  for (k = 1; k < argc; k++)
-    {
-      num = _atoi(argv[k]);
-      if (num >= 0)
+	for (k = 1; k < argc; k++)
 	{
-	  sum += num;
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
 	}
-    }
 
-  printf("%d\n", sum);
-  return (0);
+	printf("%d\n", sum);
+	return (0);
 }
