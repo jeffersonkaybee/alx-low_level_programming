81b6836702a336f6d085ef3938093bcf97d26118
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-09-30 12:25:15 -0700

    c is really fun

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The int to be operated upon
+ * author: jeffersonkaybee
+ * Return: returns an unsinged int value to calling function
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	{
+		n *= -1;
+		return (n);
+	}
+}
