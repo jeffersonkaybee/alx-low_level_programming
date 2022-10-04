dce626cd76974a70ebd53e0f9a7123152c0608f7
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-10-04 09:26:31 -0700

    c codes

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,7 +5,6 @@
  * *argstostr - concatenates all the arguments of the program
  * @ac: number of arguments
  * @av: array of arguments
- *
  * Return: Pointer to the new string (Success), NULL (Error)
  */
 char *argstostr(int ac, char **av)
@@ -22,11 +21,14 @@ char *argstostr(int ac, char **av)
 			len++;
 		len++;
 	}
+
 	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
+
 	k = 0;
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
