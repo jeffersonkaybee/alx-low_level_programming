89a00415c7f390c7fb764b91f0d7ce7552e63ef5
Jefferson Timah <jeffersonkaybee@icloud.com>
2022-10-04 09:09:51 -0700

    c codes

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,14 +22,11 @@ char *argstostr(int ac, char **av)
 			len++;
 		len++;
 	}
-
 	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
-
 	k = 0;
-
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
